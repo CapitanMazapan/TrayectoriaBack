07bddf2bd46568b6210ab19ce2bf7bb8f9e057d9
Luis Hernández <uwu.luishernandez@gmail.com>
2020-11-05 23:22:07 -0600

    add REST dashboard api and dashboard at admin

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class DashboardModel(models.Model):
+    nombre_com = models.CharField(max_length=50)
+    edad = models.CharField(max_length=10)
+    correo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_com
--- Dashboard/serializers.py
+++ Dashboard/serializers.py
@@ -1,1 +1,7 @@
-# Modelos
+from rest_framework import serializers
+from .models import DashboardModel
+
+class DashboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DashboardModel
+        fields =('__all__')
